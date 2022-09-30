db8a7800aadbfb148dc47702f6e22def21554f51
Stanislav Hanchev <stanislav@smoothr.de>
2022-09-30 15:04:25 +0300

    start

---
+++ testing/bin/dlib-19.17.0/build/temp.linux-x86_64-3.10/CMakeFiles/dlib_python.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dlib_python.
+# This may be replaced when dependencies are built.
---
+++ testing/bin/dlib-19.17.0/dlib-19.17.0/build/temp.linux-x86_64-3.10/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_1.cpp.o.d
@@ -0,0 +1,4 @@
+dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_1.cpp.o: \
+ /home/codebind/Desktop/src/university/Term\ Paper/Face_Expression_Recognition-master/testing/bin/dlib-19.17.0/dlib-19.17.0/dlib/threads/threads_kernel_1.cpp \
+ /usr/include/stdc-predef.h \
+ /home/codebind/Desktop/src/university/Term\ Paper/Face_Expression_Recognition-master/testing/bin/dlib-19.17.0/dlib-19.17.0/dlib/threads/../platform.h
