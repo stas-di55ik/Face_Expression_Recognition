31a4db66ad7d8cf4397919eb362bdb6a8c712f5e
Stanislav Hanchev <stas.hanchev@gmail.com>
2022-10-23 17:32:10 +0300

    code optimization and tgbot start

--- Edetectorgit.py
+++ Edetectorgit.py
@@ -5,33 +5,35 @@ import matplotlib.image as mpimg
 input_image = cv2.imread("Ava.png")
 emotion_detector = FER(mtcnn=True)
 
-# Save output in result variable
-result = emotion_detector.detect_emotions(input_image)
+def emotion_detection(input_image):
+    result = emotion_detector.detect_emotions(input_image)
 
-bounding_box = result[0]["box"]
-emotions = result[0]["emotions"]
-cv2.rectangle(input_image,(
-  bounding_box[0], bounding_box[1]),(
-  bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3]),
-              (0, 155, 255), 2,)
+    bounding_box = result[0]["box"]
+    emotions = result[0]["emotions"]
+    cv2.rectangle(input_image, (
+    bounding_box[0], bounding_box[1]), (
+     bounding_box[0] + bounding_box[2], bounding_box[1] + bounding_box[3]),
+                  (0, 155, 255), 2,)
 
-emotion_name, score = emotion_detector.top_emotion(input_image)
-for index, (emotion_name, score) in enumerate(emotions.items()):
-    color = (211, 211, 211) if score < 0.01 else (255, 0, 0)
-    emotion_score = "{}: {}".format(emotion_name, "{:.2f}".format(score))
+    emotion_name, score = emotion_detector.top_emotion(input_image)
+    for index, (emotion_name, score) in enumerate(emotions.items()):
+        color = (211, 211, 211) if score < 0.01 else (255, 0, 0)
+        emotion_score = "{}: {}".format(emotion_name, "{:.2f}".format(score))
 
-    cv2.putText(input_image, emotion_score,
-                (bounding_box[0], bounding_box[1] + bounding_box[3] + 30 + index * 15),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA, )
+        cv2.putText(input_image, emotion_score,
+                    (bounding_box[0], bounding_box[1] + bounding_box[3] + 30 + index * 15),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1, cv2.LINE_AA, )
 
-# Save the result in new image file
-cv2.imwrite("emotion.jpg", input_image)
+    # Save the result in new image file
+    cv2.imwrite("emotion.jpg", input_image)
 
-# Read image file using matplotlib's image module
-result_image = mpimg.imread('emotion.jpg')
-imgplot = plt.imshow(result_image)
-# Display Output Image
-plt.show()
+    # Read image file using matplotlib's image module
+    result_image = mpimg.imread('emotion.jpg')
+    imgplot = plt.imshow(result_image)
+    # Display Output Image
+    plt.axis('off')
+    plt.show()
 
+emotion_detection(input_image)
 
 
---
+++ telegramBot/TGbot.py
@@ -0,0 +1,2 @@
+import telebot
+bot = telebot.TeleBot('5798461994:AAE3Anpozgo1DQDazQmhsNK5klSfwo2A-2k')
